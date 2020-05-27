e14d871e07fd7d1a3e2a072c4c6b6c941a635ff0
MoreIceFitzGerry <maurice@fitzgerald.ch>
2020-05-27 22:06:33 +0200

    Changed Hello World String

--- tkinter-python/hello_world.py
+++ tkinter-python/hello_world.py
@@ -4,7 +4,7 @@ class HelloWorld(tk.Frame):
     def __init__(self, parent):
         super(HelloWorld, self).__init__(parent)
 
-        self.label = tk.Label(self, text="Hello, World!")
+        self.label = tk.Label(self, text="Hello, Big Fat World!")
         self.label.pack(padx=20, pady=20)
         
 if __name__ == "__main__":
